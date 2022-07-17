2e70bd30bc68097f16f6563909934187c27f928a
Bruno Alves <bruno-alves2010@live.com>
2022-07-17 17:44:09 -0300

    refactor: move all SignUp protocols to a barrel file

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,7 +1,5 @@
-import { AccountModel } from '../../domain/models';
-import { AddAccount, AddAccountModel } from '../../domain/usecases';
-import { InvalidParamError, MissingParamError, ServerError } from '../errors';
-import { EmailValidator } from '../protocols';
+import { InvalidParamError, MissingParamError, ServerError } from '../../errors';
+import { EmailValidator, AddAccount, AddAccountModel, AccountModel } from './signup-protocols';
 import { SignUpController } from './signup';
 
 const makeEmailValidator = (): EmailValidator => {
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,6 @@
-import { AddAccount } from '../../domain/usecases';
-import { InvalidParamError, MissingParamError } from '../errors';
-import { badRequest, serverError } from '../helper/http-helper';
-import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols';
+import { InvalidParamError, MissingParamError } from '../../errors';
+import { badRequest, serverError } from '../../helper/http-helper';
+import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount } from './signup-protocols';
 
 export class SignUpController implements Controller {
   constructor(
