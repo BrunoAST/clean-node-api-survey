32a8d4c12acd31c94d37fc28d8d15d170fd3c5e3
Bruno Alves <bruno-alves2010@live.com>
2022-07-24 15:46:55 -0300

    chore: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,38 @@
 module.exports = {
-  extends: 'standard-with-typescript',
-  parserOptions: {
-    project: './tsconfig.json'
-  }
-}
+	'env': {
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'@typescript-eslint/no-unused-vars': ['off'],
+		'@typescript-eslint/no-explicit-any': ['off']
+	}
+};
--- src/presentation/helper/http-helper.ts
+++ src/presentation/helper/http-helper.ts
@@ -2,22 +2,22 @@ import { ServerError } from '../errors/server-error';
 import { HttpResponse } from '../protocols/http';
 
 export const badRequest = (error: Error): HttpResponse => {
-  return {
-    statusCode: 400,
-    body: error
-  };
-}
+	return {
+		statusCode: 400,
+		body: error
+	};
+};
 
 export const serverError = (): HttpResponse => {
-  return {
-    statusCode: 500,
-    body: new ServerError()
-  };
-}
+	return {
+		statusCode: 500,
+		body: new ServerError()
+	};
+};
 
 export const ok = (data: any): HttpResponse => {
-  return {
-    statusCode: 200,
-    body: data
-  };
-}
+	return {
+		statusCode: 200,
+		body: data
+	};
+};
